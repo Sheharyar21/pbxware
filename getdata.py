7f83e46568d4217da5281b70322dfc9b008cb3e4
Sheharyar21 <88775196+Sheharyar21@users.noreply.github.com>
2023-05-31 21:47:49 +0500

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -36,11 +36,66 @@ for file_info in file_data:
     file_data_string += f"Creation Date: {file_info[2]}\n\n"
 
 
-smtp_server = 'smtp.gmail.com'
+import os
+import time 
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+file_data = []
+
+current_directory = os.getcwd()
+
+for filename in os.listdir(current_directory):
+   
+    if os.path.isfile(os.path.join(current_directory, filename)):
+
+        file_size = os.path.getsize(filename)
+      
+        creation_date = os.path.getctime(filename)
+
+        creation_date_formatted =  time.ctime(creation_date)
+        file_data.append((filename, file_size, creation_date_formatted))
+
+
+        print(f"File Name: {filename}")
+        print(f"File Size: {file_size} bytes")
+        print(f"Creation Date: {creation_date_formatted}")
+        print()
+
+
+
+file_data_string = ""
+for file_info in file_data:
+    file_data_string += f"File Name: {file_info[0]}\n"
+    file_data_string += f"File Size: {file_info[1]} bytes\n"
+    file_data_string += f"Creation Date: {file_info[2]}\n\n"
+
+
+smtp_server = 'smtp.office365.com'
 smtp_port = 587
-sender_email = 'fefurqan@gmail.com'
-receiver_email = 'furqan9651@gmail.com'
-password = 'leapord45'
+sender_email = 'no-reply@xinix.co.uk'
+receiver_email = 'nanwar@xinix.co.uk'
+password = 'Jaz35777'
+
+
+message = MIMEMultipart()
+message['From'] = sender_email
+message['To'] = receiver_email
+message['Subject'] = 'File Data in Current Directory'
+
+
+message.attach(MIMEText(file_data_string, 'plain'))
+
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.starttls()
+    server.login(sender_email, password)
+    server.send_message(message)
+
+print("Email sent successfully!")
+
 
 
 message = MIMEMultipart()
