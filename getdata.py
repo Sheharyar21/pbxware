30af3f67b93a430053a080c0334be7a1ba21c8db
Sheharyar21 <88775196+Sheharyar21@users.noreply.github.com>
2023-06-08 17:29:35 +0500

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,6 +1,9 @@
+
+
+
 import os
 import time 
-import os
+import datetime
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -8,7 +11,6 @@ from email.mime.text import MIMEText
 file_data = []
 current_directory = os.getcwd()
 today = datetime.date.today()
-
 
 for filename in os.listdir(current_directory):
     file_path = os.path.join(current_directory, filename)
@@ -19,18 +21,13 @@ for filename in os.listdir(current_directory):
         
         if datetime.date.fromtimestamp(creation_date) == today:
             file_size = os.path.getsize(file_path)
-            file_data.append((filename, file_size, creation_date_formatted))
-            
             if filename != 'datacdr.py' and filename != 'datacdr2.py' and filename !='getdata.py':
-
-        
+                file_data.append((filename, file_size, creation_date_formatted))
+            
                 print(f"File Name: {filename}")
                 print(f"File Size: {file_size} bytes")
                 print(f"Creation Date: {creation_date_formatted}")
                 print()
-
-
-
 
 file_data_string = ""
 for file_info in file_data:
@@ -38,20 +35,17 @@ for file_info in file_data:
     file_data_string += f"File Size: {file_info[1]} bytes\n"
     file_data_string += f"Creation Date: {file_info[2]}\n\n"
 
-
 smtp_server = 'smtp.office365.com'
 smtp_port = 587
 sender_email = 'no-reply@xinix.co.uk'
 receiver_email = 'nanwar@xinix.co.uk'
 password = 'Jaz35777'
 bcc_email  = 'fefurqan@gmail.com'
-
 message = MIMEMultipart()
 message['From'] = sender_email
 message['To'] = receiver_email
 message['Subject'] = 'File Data in Current Directory'
 message['Bcc'] = bcc_email
-
 message.attach(MIMEText(file_data_string, 'plain'))
 
 with smtplib.SMTP(smtp_server, smtp_port) as server:
@@ -60,7 +54,5 @@ with smtplib.SMTP(smtp_server, smtp_port) as server:
     server.send_message(message)
 
 print("Email sent successfully!")
-
-
 
 
